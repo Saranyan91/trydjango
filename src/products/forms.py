9c94b46ea5b99ee512e8070f62ea464c530196e8
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-18 16:22:28 -0500

    completed until Form validation methods

--- src/products/forms.py
+++ src/products/forms.py
@@ -4,6 +4,21 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(
+        attrs={"placeholder": "Your title"}))
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(
+                                      attrs={
+                                          "placeholder": "Your Description",
+                                          "class": "new-class-name two",
+                                          "id": "my-id-for-textarea",
+                                          "rows": 20,
+                                          "cols": 50
+                                      }))
+    price = forms.DecimalField(initial=199.99)
+
+    email = forms.EmailField()
+
     class Meta:
         model = Product
         fields = [
@@ -11,6 +26,18 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "CFE" in title:
+            raise forms.ValidationError('This is not a Valid title')
+        return title
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not a valid email")
+        return email
 
 
 class RawProductForm(forms.Form):
