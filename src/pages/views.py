797b3a3c6ca96a5724b26c70e79ed75691222d90
Saranyan Senthivel <saran.s6691@gmail.com>
2020-06-17 20:38:26 -0500

    completed until template tags and filters

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,7 +13,13 @@ def contact_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1, 2, 45, 4564564, "adhiadhgisdhg"]
+    }
+    return render(request, "about.html", my_context)
 
 
 def social_view(request, *args, **kwargs):
